45d567072788d3d6137c4785dc6ff60c5b3c7a59
kpinheirors <kpinheiro@cpqi.com>
2020-07-26 03:55:36 -0300

    AI-3.6.3 <kayor@DESKTOP-HD9PHNP Overwrite remote https://github.com/Raynerkayo/Beer2.git to local

--- app/src/main/java/br/com/beer/ui/activity/util/ConstantsActivities.java
+++
@@ -1,9 +0,0 @@
-package br.com.beer.ui.activity.util;
-
-public interface ConstantsActivities {
-
-    String KEY_BEER = "beer";
-    String REMOVE_BEER = "Remove Beer";
-    String MESSAGE = "You want to remove your favorite beer?";
-
-}
